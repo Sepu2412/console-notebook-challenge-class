1f04e9db7ffcc5ea48eb04a6024d6c9ac51b4e60
Tomas Sepulveda <sepulvedatomas1027@gmail.com>
2025-02-17 12:51:56 -0500

    avance de codigo

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,14 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+
+class Note:
+    def __init__(self, High: str, Medium: str, Low: str ):
+        self.High: str = High
+        self.Medium: str = Medium
+        self.Low: str = Low
+
+
+
+
+class Notebook:
+    def __init__(self):
