5da19d630ccb0d8270bfb003251b692c1befa73f
Tomas Sepulveda <sepulvedatomas1027@gmail.com>
2025-02-17 13:25:19 -0500

    nueva def add note

--- app/notebook.py
+++ app/notebook.py
@@ -8,16 +8,24 @@ class Note:
         self.Low:str = Low
 
     def __int__(self, code: str , tittle: str , text: str , importance: str , datetime: str):
-        self.code = code
-        self.tittle = tittle
-        self.text = text
-        self.importance = importance
-        self.creation.date = datetime.now()
+        self.code: str = code
+        self.tittle: str = tittle
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: str = datetime.now()
         self.tags = []
 
     def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"date: {self.creation_date}\n{self.tittle}: {self.text}"
 
 
 
 class Notebook:
     def __init__(self):
+        self.notes: list[Note]= []
+
+    def add_note(self, tittle: str , text: str , importance: str )-> int:
