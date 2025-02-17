e8c66af94abc06e410e1c107ed059b561e488889
Tomas Sepulveda <sepulvedatomas1027@gmail.com>
2025-02-17 13:02:41 -0500

    nueva def add

--- app/notebook.py
+++ app/notebook.py
@@ -3,10 +3,19 @@
 
 class Note:
     def __init__(self, High: str, Medium: str, Low: str ):
-        self.High: str = High
-        self.Medium: str = Medium
-        self.Low: str = Low
+        self.High:str = High
+        self.Medium:str = Medium
+        self.Low:str = Low
 
+    def __int__(self, code: str , tittle: str , text: str , importance: str , datetime: str):
+        self.code = code
+        self.tittle = tittle
+        self.text = text
+        self.importance = importance
+        self.creation.date = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag):
 
 
 
